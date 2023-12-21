58d98b997254ca5c2e47e1116e16fb337d572070
razrafart <user@email.com>
2023-12-21 09:15:51 -0800

    updated game with game logic

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const words = ["Mario", "Luigi", "Peach", "Bowser", "Toad", "Yoshi","Koopa","Goomba", 
 "Mushroom", "Star","Pipe", "Coin", "Block", "Castle", "Kart", "Race", "Princess"];
 
-let selectedLetters = []
-
 const selectedWord = words[1].toLowerCase().split('')
 
-const toggleBox = function(){
-    this.classList.toggle('.closed-box')
+let selectedLetters = []
+
+function toggleClass(letter) {
+    let elements = document.getElementById(letter);
+    elements.classList.toggle('.closed-box');
 }
 
 
@@ -16,14 +17,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     function getLetter(evt) {
         let selectedLetter = evt.target.id;
+
         //If selectedletter is not in selectedLetters list then add to list
         if (selectedLetters.includes(selectedLetter) === false){
             selectedLetters.push(selectedLetter)
             console.log(selectedLetters)
             //Toggle the class .closed-box
+            toggleClass(selectedLetter)
         }
+
         if (selectedWord.includes(selectedLetter)){
             console.log("You got one!")
+            //Add letter to bottom
+        } else {
+            console.log("Wrong!")
+            //Lose a life - Erase a mushroom
         }
         
         return selectedLetter
